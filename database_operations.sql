cf3dc00a9063b7f8a95094be3e1f53f2e3244a21
Rakesh Adhithya <rakkeshadithya00@gmail.com>
2025-10-27 09:23:52 +0530

    ecommerce oltp to olap and insights generation

---
+++ database_operations.sql
@@ -0,0 +1,157 @@
+use ECOMM;
+
+select * from bundle_items;
+select * from customers;
+select * from order_items;
+select * from order_sessions;
+select * from orders;
+select * from payments;
+select * from product_bundles;
+select * from product_price_history;
+select * from products;
+select * from returns;
+select * from sessions;
+select * from shipments;
+
+
+
+
+use OLTP;
+
+select * from bundle_items;
+select * from customers;
+select * from order_items;
+select * from order_sessions;
+select * from orders;
+select * from payments;
+select * from product_bundles;
+select * from product_price_history;
+select * from products;
+select * from returns;
+select * from sessions;
+select * from shipments;
+
+
+
+
+
+use Team3;
+
+create table dim_customers (
+    customer_id BIGINT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(150),
+    phone_new VARCHAR(50),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    signup_date DATE
+);
+
+create table dim_products (
+    product_id BIGINT PRIMARY KEY,
+    product_name VARCHAR(255),
+    category_new VARCHAR(100),
+    list_price DECIMAL(10,2),
+    cost_price DECIMAL(10,2),
+    is_active BOOLEAN,
+    created_at DATE
+);
+
+create table order_fact(        
+order_id BIGINT,            
+customer_id BIGINT,          
+order_date DATE, 
+status VARCHAR(20),
+order_placed_at DATETIME ,
+order_item_id BIGINT,       
+product_id BIGINT ,         
+quantity INT,
+unit_price DECIMAL(10,2) , 
+line_total DECIMAL(12,2),
+payment_id BIGINT,          
+payment_method VARCHAR(50),
+amount DECIMAL(12,2),
+payment_date DATE
+);
+
+show tables;
+
+truncate dim_customers;
+truncate dim_products;
+truncate order_fact;
+
+select * from dim_customers;
+select * from dim_products;
+select * from order_fact;
+
+
+
+-- Insights
+-- 1. no. of orders per customer
+SELECT 
+	dc.customer_id, 
+    dc.first_name,
+    dc.last_name,
+	COUNT(odf.order_id) as total_orders
+FROM dim_customers dc
+JOIN order_fact odf ON dc.customer_id = odf.customer_id
+GROUP BY dc.customer_id, dc.first_name, dc.last_name
+ORDER BY total_orders DESC;
+
+-- 2. most selling products (quantity not price)
+SELECT
+	dp.product_id,
+    dp.product_name,
+    SUM(odf.quantity) AS total_sold
+FROM dim_products dp
+JOIN order_fact odf ON dp.product_id = odf.product_id
+GROUP BY dp.product_id, dp.product_name
+ORDER BY total_sold DESC;
+
+
+-- 3. orders per month
+SELECT
+	YEAR(order_date) AS year,
+    MONTH(order_date) AS month,
+    COUNT(order_id) AS total_orders
+FROM order_fact
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+-- 4. most selling categories
+SELECT 
+	dp.category_new,
+    SUM(odf.quantity) AS total_sold
+FROM dim_products dp
+JOIN order_fact odf ON dp.product_id = odf.product_id
+GROUP BY dp.category_new
+ORDER BY total_sold DESC;
+
+-- 5. revenue per category
+SELECT 
+	dp.category_new,
+    SUM(odf.line_total) AS total_revenue
+FROM dim_products dp
+JOIN order_fact odf ON dp.product_id = odf.product_id
+GROUP BY dp.category_new
+ORDER BY total_revenue DESC;
+
+-- 6. total revenue per each month
+SELECT
+	YEAR(order_date) AS year,
+    MONTH(order_date) AS month,
+    SUM(line_total) AS total_revenue
+FROM order_fact
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY year, month;
+
+-- 7. difference between revenue of current month with previous month
+SELECT
+	YEAR(order_date) AS year,
+    MONTH(order_date) AS month,
+	SUM(line_total) AS current_month_revenue,
+    SUM(line_total) - LAG(SUM(line_total), 1, 0) OVER (ORDER BY year(order_date), month(order_date)) AS diff_from_prev
+FROM order_fact
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY year, month
--- main.py
+++ main.py
@@ -3,116 +3,98 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 oltp_config = {
-    'host' : '',                  #removed for security, as this is public repository
-    'user' : 'team3',
-    'password' : 't3ssss_DP',
-    'database' : 'OLTP'
+    'host': '35.222.34.151',
+    'user': 'team3',
+    'password': 't3ssss_DP',
+    'database': 'OLTP'
 }
 
 olap_config = {
-    'host' : '',                 #removed for security, as this is public repository
-    'user' : 'team3',
-    'password' : 't3ssss_DP',
-    'database' : 'Team3'
+    'host': '35.222.34.151',
+    'user': 'team3',
+    'password': 't3ssss_DP',
+    'database': 'Team3'
 }
 
 def get_connection(config):
-    return msc.connect(**config)
+    try:
+        return msc.connect(**config)
+    except msc.Error as e:
+        print('Error connecting to database server', e)
+        exit()
 
 def injest_data():
     con = get_connection(oltp_config)
     cur = con.cursor()
 
-    cur.execute(
-    '''
-    select * from customers
-    '''
-    )
-    customers = pd.DataFrame(cur.fetchall(), columns = ['customer_id', 'first_name', 'last_name', 'email', 'phone', 'phone_norm', 'city', 'state', 'signup_date', 'is_active', 'external_id', 'guest_flag', 'created_at', 'updated_at'])
+    cur.execute('select * from customers')
+    customers = pd.DataFrame(cur.fetchall(), columns=['customer_id', 'first_name', 'last_name', 'email', 'phone', 'phone_norm', 'city', 'state', 'signup_date', 'is_active', 'external_id', 'guest_flag', 'created_at', 'updated_at'])
     print(customers)
 
-
-    cur.execute(
-    '''
-    select * from products
-    '''
-    )
-    products = pd.DataFrame(cur.fetchall(), columns = ['product_id', 'product_name', 'product_category_raw', 'product_category', 'list_price', 'cost_price', 'is_active', 'created_at', 'updated_at'])
+    cur.execute('select * from products')
+    products = pd.DataFrame(cur.fetchall(), columns=['product_id', 'product_name', 'product_category_raw', 'product_category', 'list_price', 'cost_price', 'is_active', 'created_at', 'updated_at'])
     print(products)
 
-    cur.execute(
-    '''
-    select * from orders
-    '''
-    )
-    orders = pd.DataFrame(cur.fetchall(), columns = ['order_id', 'customer_id', 'order_date', 'status', 'order_placed_at', 'payment_initiated_at', 'payment_completed_at', 'shipped_at', 'delivered_at', 'channel', 'user_agent', 'device_type', 'utm_source', 'coupon_code', 'applied_promo_id', 'order_value', 'num_items', 'ship_address_id', 'bill_address_id', 'created_at', 'updated_at' ])
+    cur.execute('select * from orders')
+    orders = pd.DataFrame(cur.fetchall(), columns=['order_id', 'customer_id', 'order_date', 'status', 'order_placed_at', 'payment_initiated_at', 'payment_completed_at', 'shipped_at', 'delivered_at', 'channel', 'user_agent', 'device_type', 'utm_source', 'coupon_code', 'applied_promo_id', 'order_value', 'num_items', 'ship_address_id', 'bill_address_id', 'created_at', 'updated_at'])
     print(orders)
 
-
-    cur.execute(
-    '''
-    select * from order_items
-    '''
-    )
-    order_items = pd.DataFrame(cur.fetchall(), columns = ['order_item_id', 'order_id', 'product_id', 'quantity','unit_price','line_total', 'is_bundle_item', 'bundle_id'])
+    cur.execute('select * from order_items')
+    order_items = pd.DataFrame(cur.fetchall(), columns=['order_item_id', 'order_id', 'product_id', 'quantity', 'unit_price', 'line_total', 'is_bundle_item', 'bundle_id'])
     print(order_items)
 
-
-    cur.execute(
-    '''
-    select * from returns
-    '''
-    )
-    returns = pd.DataFrame(cur.fetchall(), columns = ['return_id', 'order_id', 'order_item_id', 'returned_date', 'reason_code','refund_amount', 'processed_by', 'processed_at'])
-    print(returns)
-
-
-    cur.execute(
-    '''
-    select * from payments
-    '''
-    )
-    payments = pd.DataFrame(cur.fetchall(), columns = ['payment_id', 'order_id', 'payment_method', 'amount', 'payment_date', 'is_refunded', 'refund_txn_id'])
+    cur.execute('select * from payments')
+    payments = pd.DataFrame(cur.fetchall(), columns=['payment_id', 'order_id', 'payment_method', 'amount', 'payment_date', 'is_refunded', 'refund_txn_id'])
     print(payments)
-    return customers, products, orders, order_items, returns, payments
+
+    return customers, products, orders, order_items, payments
 
 def clean_phone(ph):
     s = ''
     for c in ph:
         if c.isdigit():
-            s+= c
+            s += c
     s = s[-10:]
     return s
 
-
-category_map = {'ks' : 'books', 'eau':'beauty', 'me':'home', 'ery': 'grocery', 'rts': 'sports', 'ect': 'electronics', 'ys': 'toys', 'rel': 'apparel'}
+category_map = {'ks': 'books', 'eau': 'beauty', 'me': 'home', 'ery': 'grocery', 'rts': 'sports', 'ect': 'electronics', 'ys': 'toys', 'rel': 'apparel'}
 def clean_category(ctg):
-    for k,v in category_map.items():
+    for k, v in category_map.items():
         if k.lower() in ctg.lower():
             return v
+    return 'Unknown'
 
 def transform_data(customers, products):
     customers['phone_new'] = customers['phone'].apply(clean_phone)
     products['category_new'] = products['product_category_raw'].apply(clean_category)
     return customers, products
 
+def load_data(customers, products, orders, order_items, payments):
+    dim_customers = customers[['customer_id', 'first_name', 'last_name', 'email', 'phone_new', 'city', 'state', 'signup_date']]
+    dim_customers = dim_customers[dim_customers['customer_id'] != 0]
+    dim_customers = dim_customers[dim_customers['customer_id'].notna()]
 
-def load_data(customers, products,  orders, order_items, returns, payments):
-    con = get_connection(olap_config)
-    dim_customers = customers[['customer_id', 'first_name', 'last_name', 'email', 'phone_new','city', 'state', 'signup_date']]
     dim_products = products[['product_id', 'product_name', 'category_new', 'list_price', 'cost_price', 'is_active', 'created_at']]
-    order_fact = pd.merge(orders, order_items, on = ['order_id'], how='left')
-    order_fact = pd.merge(order_fact, returns, on = ['order_id'], how='left')
-    order_fact = pd.merge(order_fact, payments, on = ['order_id'], how='left')
-    print(order_fact)
+    dim_products = dim_products[dim_products['product_id'] != 0]
+    dim_products = dim_products[dim_products['product_id'].notna()]
 
+    order_fact = pd.merge(orders, order_items, on=['order_id'], how='inner')
+    order_fact = pd.merge(order_fact, payments, on=['order_id'], how='inner')
+    order_fact = order_fact[['order_id', 'customer_id', 'order_date', 'status', 'order_placed_at', 
+                             'order_item_id', 'product_id', 'quantity', 'unit_price', 'line_total', 
+                             'payment_id', 'payment_method', 'amount', 'payment_date']]
+    order_fact = order_fact[order_fact['order_id'] != 0]
+    order_fact = order_fact[order_fact['order_id'].notna()]
+    
     engine = create_engine('mysql+pymysql://team3:t3ssss_DP@35.222.34.151:3306/Team3')
-    dim_products.to_sql('dim_products', engine, if_exists='append', index=False)
-    dim_customers.to_sql('dim_customers', engine, if_exists='append', index=False)
-    order_fact = order_fact[['order_id', 'customer_id', 'order_date', 'status', 'order_placed_at', 'channel', 'order_value', 'num_items', 'order_item_id', 'product_id', 'quantity', 'unit_price', 'line_total', 'payment_id', 'payment_method', 'amount', 'payment_date', 'is_refunded', 'returned_id', 'return_date', 'reason_code', 'refund_amount']]
-    order_fact.to_sql('order_fact', engine, if_exists='append', index=False)
-    print('Loading Complete!   ')
+    try:
+        dim_products.to_sql('dim_products', engine, if_exists='append', index=False)
+        dim_customers.to_sql('dim_customers', engine, if_exists='append', index=False)
+        order_fact.to_sql('order_fact', engine, if_exists='append', index=False)
+        print('Loading Complete!')
+    except Exception as e:
+        print('Error loading data!', e)
 
-customers, products, orders, order_items, returns, payments = injest_data()
+customers, products, orders, order_items, payments = injest_data()
 customers, products = transform_data(customers, products)
-load_data(customers, products, orders, order_items, returns, payments)
+load_data(customers, products, orders, order_items, payments)
